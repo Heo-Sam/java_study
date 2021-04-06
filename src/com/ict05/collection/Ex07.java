aee6ad6a1edf5beb8683e288388ebad9316c0c05
Heo-Sam <toal2321@naver.com>
2021-04-06 15:40:34 +0900

    Queue: LinkedList

--- src/com/ict05/collection/Ex07.java
+++ src/com/ict05/collection/Ex07.java
@@ -9,6 +9,45 @@ public class Ex07 {
 		
 		LinkedList<String> linkedList = new LinkedList<String>();
 				
-		// 추가, 삽입: add, addFirst, 
+		// 추가, 삽입: add, addFirst, addLast, offer, offerFirst, offerLast
+		linkedList.add("박찬호");
+		linkedList.offer("박세리");
+		linkedList.add("김미현");
+		System.out.println(linkedList);
+		
+		linkedList.addFirst("김광현");
+		System.out.println(linkedList);
+		
+		linkedList.offerFirst("둘리");
+		System.out.println(linkedList);
+		
+		if (linkedList.contains("박세리")) {
+			System.out.println(linkedList.indexOf("박세리")+ "번째 위치");
+			System.out.println(linkedList.get(linkedList.indexOf("박세리")));
+			System.out.println(linkedList.getFirst());
+			System.out.println(linkedList.getLast());
+		} 
+		System.out.println(linkedList.size() + "요소가 존재함");
+		
+		
+		//둘리를 이대호로 바꼬라
+		if(linkedList.contains("둘리")) {
+			linkedList.set(linkedList.indexOf("둘리"), "이대호");
+		} else {
+			System.out.println("변경할 요소가 없다.");
+		}
+		System.out.println(linkedList);
+		
+		
+		// 삭제 : remove, removeFirst, removeLast
+		linkedList.removeFirst();
+		System.out.println(linkedList);
+		
+		linkedList.removeLast();
+		System.out.println(linkedList);
+		
+		// remove(int index) : String -> 삭제되면서 삭제된 객체가 출력됨
+		System.out.println(linkedList.remove(1) + "님, 삭제되었습니다.");
+		System.out.println(linkedList);
 	}
 }
